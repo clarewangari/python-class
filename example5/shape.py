2c889934100f23ba052df15355670407ad436103
clarewangari <47854393+clarewangari@users.noreply.github.com>
2019-04-30 19:26:21 +0300

    Add files via upload

---
+++ example5/shape.py
@@ -0,0 +1,50 @@
+class Circle:
+	def __init__(self,radius):
+		self.radius = radius
+		self.length =[]
+		self.answer = []
+	def area(self,radius):
+		self.radius = area/self.radius*22/7*self.radius
+		print("A=22/7*r*r")
+
+class Square:
+	def __init__(self,side):
+		self.side = side
+		self.answer =[]
+	def area(self, answer):
+		 self.answer = a*a
+		 print(A=a*a)
+	def perimeter(self,width):
+		self.answer = 4(a+a+a+a)
+		print("p=4a")
+
+class Rectangle:
+	def __init__(self,width,length):
+		self.width = width
+		self.length = length
+		#self.answer = []
+	def area(self,answer):
+		self.answer = self.width*self.length
+		print("A=W*l")
+	def perimeter(self):
+		self.answer = 2(self.length + self.width)
+		print("P=2(L+W")
+
+
+
+
+class Sphere:
+	def __init__(self,radius):
+		self.radius = radius
+		self.answer =[]
+	def surface_area(self,radius):
+		self.answer = 4*3.143*radius*radius
+		print("A=4*3*r*r")
+	def volume(self):
+		self.answer = 4/3(3.143*radius*radius*radius)
+		print("v=4/3(3.143*r*r*r")
+
+
+
+
+
