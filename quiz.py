b781be1f6338b254a3845526111e2af6385fd865
clarewangari <47854393+clarewangari@users.noreply.github.com>
2019-05-07 19:14:43 +0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,55 @@
+
+tens = [1,2,3,4,5,6,7,8,9]
+for ten in tens:
+	x*10
+	print ("answer")
+
+
+x = [2,4,6,8,10,12]
+	x*x
+	print("answer")
+
+
+sorted = [9,1,4,7,3] + [5,2,6,8,0]
+	sorted.sort()
+	print("a sorted list")
+
+
+
+range_sum = [1,2,3,4]
+	
+	print ("the sum of numbers is", len(range_sum))
+
+
+
+largest = []:
+	([1,3,6,8,2,4,5])
+	largest.sort()
+	print ("largest number is", largest[0])
+
+
+
+smallest =[] :
+	([1,2,3,4,5,4,5])
+	smallest.sort()
+	print ("smallest number is",smallest[0])
+
+my_modules = range(10,20)
+	for x in my_modules:
+	print(x,end = " ")
+
+
+
+ 
+
+students = [
+	{"balance": 1000, "name":"irene"}
+	{"balance": 2000, "name": "pauline"}
+	{"balance": 3000,"name": "naima"}
+	{"balance": 1000, "name": "rose"}
+]
+	for balance, name in students:
+		print("hello {} your balance is {}".format(balnce,name))
+
+
+
